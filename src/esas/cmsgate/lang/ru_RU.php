77afc377470f33aab382b3fba831a01bda6f9b17
n.mekh <nikita.mekh@gmail.com>
2020-05-21 13:57:46 +0300

    EPOSCG-5: initial commit

--- src/esas/cmsgate/lang/ru_RU.php
+++ src/esas/cmsgate/lang/ru_RU.php
@@ -4,5 +4,9 @@ use esas\cmsgate\messenger\MessagesBitrix;
 
 
 return array(
-    MessagesBitrix::ERROR_PS_INSTALL => "Ошибка работы модуля",
+    MessagesBitrix::ERROR_PS_INSTALL => "Не удалось создать платёжную систему",
+    MessagesBitrix::ERROR_SALE_MODULE_NOT_INSTALLED => "Для работы модуля необходим модуль интернет-магазина (sale)",
+    MessagesBitrix::ERROR_ORDERS_EXIST => "Невозможно удалить платежную систему, т.к. существуют заказы с данной платёжной системой.",
+    MessagesBitrix::ERROR_DELETE_EXCEPTION => "Невозможно удалить платежную систему.",
+    MessagesBitrix::ERROR_PS_ACTION_REG => "Ошибка регистрации обработчиков ПСОшибка регистрации обработчиков ПС",
 );
