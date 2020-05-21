202f0dd23d9c0b7fe70d694fd4495d6ffc1ed34c
n.mekh <nikita.mekh@gmail.com>
2020-05-21 13:21:17 +0300

    Initial commit (дополнение)

---
+++ src/esas/cmsgate/messenger/MessagesBitrix.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: nikit
+ * Date: 18.02.2020
+ * Time: 11:11
+ */
+
+namespace esas\cmsgate\messenger;
+
+
+class MessagesBitrix extends Messages
+{
+    const ERROR_PS_INSTALL = 'ERROR_PS_INSTALL';
+    const ERROR_PS_ACTION_REG = 'ERROR_PS_ACTION_REG';
+    const ERROR_SALE_MODULE_NOT_INSTALLED = 'ERROR_SALE_MODULE_NOT_INSTALLED';
+    const ERROR_ORDERS_EXIST = 'ERROR_ORDERS_EXIST';
+    const ERROR_DELETE_EXCEPTION = 'ERROR_DELETE_EXCEPTION';
+
+}
