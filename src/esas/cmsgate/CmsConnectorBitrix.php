bcf1c1db920c07235e6c5593be81d8d005599105
n.mekh <nikita.mekh@gmail.com>
2020-08-07 13:14:22 +0300

    Имена tab-в в админке

--- src/esas/cmsgate/CmsConnectorBitrix.php
+++ src/esas/cmsgate/CmsConnectorBitrix.php
@@ -113,8 +113,8 @@ class CmsConnectorBitrix extends CmsConnector
         return new CmsConnectorDescriptor(
             "cmsgate-bitrix-lib",
             new VersionDescriptor(
-                "v1.11.4",
-                "2020-07-24"
+                "v1.11.5",
+                "2020-08-24"
             ),
             "Cmsgate Bitrix connector",
             "https://bitbucket.esas.by/projects/CG/repos/cmsgate-bitrix-lib/browse",
--- src/esas/cmsgate/bitrix/CmsgateCModuleBitrix24.php
+++ src/esas/cmsgate/bitrix/CmsgateCModuleBitrix24.php
@@ -8,11 +8,27 @@
 
 namespace esas\cmsgate\bitrix;
 
+use esas\cmsgate\Registry;
+
 class CmsgateCModuleBitrix24 extends CmsgateCModule
 {
     protected function getPaysystemType()
     {
         return "CRM_INVOICE";
+    }
+
+    function InstallEvents()
+    {
+        parent::UnInstallEvents();
+        $eventManager = \Bitrix\Main\EventManager::getInstance();
+        $eventManager->registerEventHandler('sale', 'OnGetBusinessValueGroups', Registry::getRegistry()->getModuleDescriptor()->getModuleMachineName(), CmsgateEventHandler::class, 'onGetBusValueGroups');
+    }
+
+    function UnInstallEvents()
+    {
+        parent::UnInstallEvents();
+        $eventManager = \Bitrix\Main\EventManager::getInstance();
+        $eventManager->unRegisterEventHandler('sale', 'OnGetBusinessValueGroups', Registry::getRegistry()->getModuleDescriptor()->getModuleMachineName(), CmsgateEventHandler::class, 'onGetBusValueGroups');
     }
 
 
